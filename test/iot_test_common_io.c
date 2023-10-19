ed0e3f32500d416c43f1f20a10c1bb7bda7dba72
Patrick Cook <cookpate@amazon.com>
2023-10-19 15:10:21 -0700

    Change ifdef blocks to if SUPPORTED > 0
    iot_test_common_io_internal defines any missing, #ifdef is always true.
    SUPPORTED macros from the implementation test config.
    This also fixes reliance on always-false or link-time optimizations
    to prevent linkage errors against unimplemented common-io components.

--- test/iot_test_common_io.c
+++ test/iot_test_common_io.c
@@ -56,7 +56,7 @@ TEST_GROUP_RUNNER( Common_IO )
 {
     size_t i = 0;
 
-    #ifdef IOT_TEST_COMMON_IO_PERFCOUNTER_SUPPORTED
+    #if IOT_TEST_COMMON_IO_PERFCOUNTER_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_PERFCOUNTER_SUPPORTED; i++ )
         {
             SET_TEST_IOT_PERFCOUNTER_CONFIG( i );
@@ -65,7 +65,7 @@ TEST_GROUP_RUNNER( Common_IO )
     #endif
 
     /* These already used loop back tests which require minimum of two pins */
-    #ifdef IOT_TEST_COMMON_IO_GPIO_SUPPORTED
+    #if IOT_TEST_COMMON_IO_GPIO_SUPPORTED > 0
         for( i = 1; i < IOT_TEST_COMMON_IO_GPIO_SUPPORTED; i++ )
         {
             SET_TEST_IOT_GPIO_CONFIG( i );
@@ -73,7 +73,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_UART_SUPPORTED
+    #if IOT_TEST_COMMON_IO_UART_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_UART_SUPPORTED; i++ )
         {
             SET_TEST_IOT_UART_CONFIG( i );
@@ -81,7 +81,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_I2C_SUPPORTED
+    #if IOT_TEST_COMMON_IO_I2C_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_I2C_SUPPORTED; i++ )
         {
             SET_TEST_IOT_I2C_CONFIG( i );
@@ -89,7 +89,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_SPI_SUPPORTED
+    #if IOT_TEST_COMMON_IO_SPI_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_SPI_SUPPORTED; i++ )
         {
             SET_TEST_IOT_SPI_CONFIG( i );
@@ -97,7 +97,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_WATCHDOG_SUPPORTED
+    #if IOT_TEST_COMMON_IO_WATCHDOG_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_WATCHDOG_SUPPORTED; i++ )
         {
             SET_TEST_IOT_WATCHDOG_CONFIG( i );
@@ -105,7 +105,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_RTC_SUPPORTED
+    #if IOT_TEST_COMMON_IO_RTC_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_RTC_SUPPORTED; i++ )
         {
             SET_TEST_IOT_RTC_CONFIG( i );
@@ -113,7 +113,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_RESET_SUPPORTED
+    #if IOT_TEST_COMMON_IO_RESET_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_RESET_SUPPORTED; i++ )
         {
             SET_TEST_IOT_RESET_CONFIG( i );
@@ -121,7 +121,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_TIMER_SUPPORTED
+    #if IOT_TEST_COMMON_IO_TIMER_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_TIMER_SUPPORTED; i++ )
         {
             SET_TEST_IOT_TIMER_CONFIG( i );
@@ -129,7 +129,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_FLASH_SUPPORTED
+    #if IOT_TEST_COMMON_IO_FLASH_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_FLASH_SUPPORTED; i++ )
         {
             SET_TEST_IOT_FLASH_CONFIG( i );
@@ -137,7 +137,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_ADC_SUPPORTED
+    #if IOT_TEST_COMMON_IO_ADC_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_ADC_SUPPORTED; i++ )
         {
             SET_TEST_IOT_ADC_CONFIG( i );
@@ -145,7 +145,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_PWM_SUPPORTED
+    #if IOT_TEST_COMMON_IO_PWM_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_PWM_SUPPORTED; i++ )
         {
             SET_TEST_IOT_PWM_CONFIG( i );
@@ -153,7 +153,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_I2S_SUPPORTED
+    #if IOT_TEST_COMMON_IO_I2S_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_I2S_SUPPORTED; i++ )
         {
             SET_TEST_IOT_I2S_CONFIG( i );
@@ -161,7 +161,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_EFUSE_SUPPORTED
+    #if IOT_TEST_COMMON_IO_EFUSE_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_EFUSE_SUPPORTED; i++ )
         {
             SET_TEST_IOT_EFUSE_CONFIG( i );
@@ -169,7 +169,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_SDIO_SUPPORTED
+    #if IOT_TEST_COMMON_IO_SDIO_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_SDIO_SUPPORTED; i++ )
         {
             SET_TEST_IOT_SDIO_CONFIG( i );
@@ -177,7 +177,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_TEMP_SENSOR_SUPPORTED
+    #if IOT_TEST_COMMON_IO_TEMP_SENSOR_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_TEMP_SENSOR_SUPPORTED; i++ )
         {
             SET_TEST_IOT_TEMP_SENSOR_CONFIG( i );
@@ -185,7 +185,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_POWER_SUPPORTED
+    #if IOT_TEST_COMMON_IO_POWER_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_POWER_SUPPORTED; i++ )
         {
             SET_TEST_IOT_POWER_CONFIG( i );
@@ -193,7 +193,7 @@ TEST_GROUP_RUNNER( Common_IO )
         }
     #endif
 
-    #ifdef IOT_TEST_COMMON_IO_USB_DEVICE_SUPPORTED
+    #if IOT_TEST_COMMON_IO_USB_DEVICE_SUPPORTED > 0
         for( i = 0; i < IOT_TEST_COMMON_IO_USB_DEVICE_SUPPORTED; i++ )
         {
             SET_TEST_IOT_USB_DEVICE_CONFIG( i );
